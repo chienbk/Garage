675a9666f92d079ccfe0c0e755e3808d9accbbe8
Nguyen Van Chien <nvc.hut@gmail.com>
2018-07-01 16:02:40 +0700

    First commit

---
+++ app/src/main/java/thebrightcompany/com/garage/view/SplashActivity.java
@@ -0,0 +1,46 @@
+package thebrightcompany.com.garage.view;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import thebrightcompany.com.garage.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final String TAG = SplashActivity.class.getSimpleName();
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                //Intent i = new Intent(SplashActivity.this, LoginScreenActivity.class);
+                //startActivity(i);
+
+                // close this activity
+                //finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
