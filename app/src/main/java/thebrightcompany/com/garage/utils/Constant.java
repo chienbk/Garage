67c550fad148b501bead5981c99161e7140612ce
Nguyen Van Chien <nvc.hut@gmail.com>
2018-07-05 22:54:05 +0700

    Update class Constant to defind api

---
+++ app/src/main/java/thebrightcompany/com/garage/utils/Constant.java
@@ -0,0 +1,17 @@
+package thebrightcompany.com.garage.utils;
+
+public class Constant {
+
+    public static final String ROOT_URL = "";
+    public static final String URL_LOGIN = "v1/gara/login";
+    public static final String URL__CUSTOMER_ON_MAP = "v1/gara/customer-on-map/{idOfGara}";
+    public static final String URL_UPDATE_STATE = "v1/gara/update-state";
+    public static final String URL_SEARCH_USER = "v1/gara/search-user";
+    public static final String URL_CREATE_ORDER = "v1/gara/add-order";
+    public static final String URL_GET_CUSTOMER = "v1/gara/get-customers/{idOfGara}/{state}";
+    public static final String URL_GET_LIST_NOTIFICATION = "v1/gara/notification/{idOfGara}";
+    public static final String URL_UPDATE_TIME = "v1/gara/update-time-finish";
+    public static final String URL_PAYMENT = "v1/gara/update-payment/{idOfOrder}";
+    public static final String URL_ORDER_DETAIL = "v1/gara/order-detail/{idOfOrder}";
+    public static final String URL_PROFILE = "v1/gara/profile/{idOfUser}";
+}
