20c671988d82d3bf8ef3d7498e616a5808bb545b
Nguyen Van Chien <nvc.hut@gmail.com>
2018-07-06 00:57:06 +0700

    Update code

--- app/src/main/java/thebrightcompany/com/garage/view/login/LoginActivity.java
+++ app/src/main/java/thebrightcompany/com/garage/view/login/LoginActivity.java
@@ -2,10 +2,13 @@ package thebrightcompany.com.garage.view.login;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -120,13 +123,12 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
     }
 
     @OnClick(R.id.btn_login)
-    public void processLogin(String emailUser, String passwordUser){
+    public void processLogin(){
         //Get data from UI
-        emailUser = txt_email.getText().toString();
-        passwordUser = txt_password.getText().toString();
-        this.email = emailUser;
-        this.password = passwordUser;
+        email = txt_email.getText().toString();
+        password = txt_password.getText().toString();
         showProgress();
+        createTimeOut(3000);
         //todo process login
 
     }
@@ -144,5 +146,27 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
                 onLoginSuccess("");
             }
         }, time );
+    }
+
+    /**
+     * The method used to hide the keyboard
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
